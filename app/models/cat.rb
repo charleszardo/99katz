226f38342bf5006383db239ad169e4e5d146a942
Dave Charnuska <davecharnuska@gmail.com>
2016-12-01 02:10:10 -0500

    add sex validation

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,8 +1,10 @@
 class Cat < ActiveRecord::Base
   COLORS = %w{ red orange yellow green blue indigo violet }
+  GENDERS = %{ O F M }
 
   validates :birth_date, :color, :name, :sex, presence: true
-  validates :color, inclusion: { in: COLORS, message: "%{value} is not a valid color"}
+  validates :color, inclusion: { in: COLORS, message: "%{value} is not a valid color" }
+  validates :gender, inclusion: { in: GENDERS, message: "%{value} is not a valid option" }
 
   def age
     Integer(Integer(Date.today - birth_date) / 365.25)
