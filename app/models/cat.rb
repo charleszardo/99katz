cc18aad639fb5cd84a77b6e27ebf9528ca397dc0
Dave Charnuska <davecharnuska@gmail.com>
2016-12-01 02:02:47 -0500

    add Cat#age

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,6 +1,10 @@
 class Cat < ActiveRecord::Base
+  COLORS = %w{ red orange yellow green blue indigo violet }
+
   validates :birth_date, :color, :name, :sex, presence: true
   validates :color, inclusion: { in: COLORS, message: "%{value} is not a valid color"}
 
-  COLORS = %w{ red orange yellow green blue indigo violet }
+  def age
+    Integer(Integer(Date.today - birth_date) / 365.25)
+  end
 end
