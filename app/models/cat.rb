58a118f85cc84cd2cd6e70c7536c922e9993e9ae
Dave Charnuska <davecharnuska@gmail.com>
2016-12-01 23:48:48 -0500

    add Cat class constants getters

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -6,6 +6,14 @@ class Cat < ActiveRecord::Base
   validates :color, inclusion: { in: COLORS, message: "%{value} is not a valid color" }
   validates :sex, inclusion: { in: GENDERS, message: "%{value} is not a valid option" }
 
+  def self.get_genders
+    GENDERS
+  end
+
+  def self.get_colors
+    COLORS
+  end
+
   def age
     Integer(Integer(Date.today - birth_date) / 365.25)
   end
