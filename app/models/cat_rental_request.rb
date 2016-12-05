9897d15527bba957c10a734b4b590aec82e8cb88
Dave Charnuska <davecharnuska@gmail.com>
2016-12-04 22:26:47 -0500

    add approved_requests_cannot_overlap validation

--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -3,6 +3,7 @@ class CatRentalRequest < ActiveRecord::Base
 
   validates :cat_id, :start_date, :end_date, :status, presence: true
   validates :status, inclusion: STATUSES
+  validate :approved_requests_cannot_overlap
 
   belongs_to :cat
 
@@ -13,11 +14,19 @@ class CatRentalRequest < ActiveRecord::Base
       .where('start_date < ? OR end_date > ?', self.end_date, self.start_date)
   end
 
+  private
+
   def overlapping_approved_requests
     overlapping_requests.where("status = 'APPROVED'")
   end
 
   def overlapping_pending_requests
     overlapping_requests.where("status = 'PENDING'")
+  end
+
+  def approved_requests_cannot_overlap
+    if self.status == 'APPROVED' && !overlapping_approved_requests.empty?
+      errors[:approval] << 'Cat rental already approved for this time'
+    end
   end
 end
