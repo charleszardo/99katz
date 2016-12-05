daf12ae4f59e6037fd3ef4bd15f858c38d448898
Dave Charnuska <davecharnuska@gmail.com>
2016-12-04 23:00:31 -0500

    add create action to CRR controller

--- app/controllers/cat_rental_requests_controller.rb
+++ app/controllers/cat_rental_requests_controller.rb
@@ -5,4 +5,21 @@ class CatRentalRequestsController < ApplicationController
 
     render :new
   end
+
+  def create
+    @request = CatRentalRequest.new(request_params)
+    cat = Cat.find(request_params.cat_id)
+
+    if @request.save
+      redirect_to cat_url(cat)
+    else
+      flash.now[:errors] = @request.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+  def request_params
+    params.require(:cat_rental_request).permit(:cat_id, :start_date, :end_date)
+  end
 end
