b2ba25233625d771b537b5ad08cc8cafda8d2b4c
Dave Charnuska <davecharnuska@gmail.com>
2016-12-06 16:13:35 -0500

    add User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true, uniqueness: true
+  validates :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: { message: "Password can't be blank" }
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  before_validation :ensure_session_token
+
+  def self.generate_session_token
+    SecureRandom::Base64(16)
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
 end
