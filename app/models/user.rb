31306e2b1b15301cab3db67e9fcfafefe4dd02b2
Dave Charnuska <davecharnuska@gmail.com>
2016-12-06 16:27:06 -0500

    User model: add #password, ::find_by_credentials, #password=, #password_digest, #is_password?, #reset_session_token!; fix ::generate_session_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,38 @@ class User < ActiveRecord::Base
 
   before_validation :ensure_session_token
 
+  attr_accessor :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+
+    !user.nil? && user.password_digest.is_password?(password) ? user : nil
+  end
+
   def self.generate_session_token
-    SecureRandom::Base64(16)
+    SecureRandom::base64(16)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def password_digest
+    BCrypt::Password.new(super)
+  end
+
+  def is_password?(password)
+    self.password_digest.is_password?(password)
   end
 
   private
   def ensure_session_token
     self.session_token ||= User.generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save
   end
 end
