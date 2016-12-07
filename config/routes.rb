a23cb7a613fff8a6bfea932ac6f1f50c3fdd5b12
Dave Charnuska <davecharnuska@gmail.com>
2016-12-07 15:18:13 -0500

    fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -63,5 +63,5 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
-  resource :session, only: [:new, :create:, :destroy]
+  resource :session, only: [:new, :create, :destroy]
 end
