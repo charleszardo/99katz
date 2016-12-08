76ee4098ea52e82eea207d7f54bda142cbc46adf
Dave Charnuska <davecharnuska@gmail.com>
2016-12-08 17:20:37 -0500

    display active sessions on User show view

--- config/routes.rb
+++ config/routes.rb
@@ -61,7 +61,7 @@ Rails.application.routes.draw do
     post "deny", on: :member
   end
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
 
   resource :session, only: [:new, :create, :destroy]
 end
