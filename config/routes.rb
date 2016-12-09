771063b88da409a1cb190c136335ba1d117a1f28
Dave Charnuska <davecharnuska@gmail.com>
2016-12-09 01:20:28 -0500

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root 'cats#index'
 
   resources :cats, except: [:destroy]
 
