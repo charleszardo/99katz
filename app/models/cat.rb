1f272273ec5edd8c577f63b4d853c5b80601b65d
Dave Charnuska <davecharnuska@gmail.com>
2016-12-09 11:30:45 -0500

    add birth_date_in_the_past validation to Cat model

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -7,6 +7,7 @@ class Cat < ActiveRecord::Base
   validates :birth_date, :color, :name, :sex, :owner, presence: true
   validates :color, inclusion: COLORS
   validates :sex, inclusion: GENDERS
+  validate :birth_date_in_the_past
 
   has_many :cat_rental_requests, :dependent => :destroy
   belongs_to :owner, foreign_key: "user_id", class_name: "User"
@@ -21,5 +22,13 @@ class Cat < ActiveRecord::Base
 
   def age
     time_ago_in_words(birth_date)
+  end
+
+  private
+
+  def birth_date_in_the_past
+    if birth_date && birth_date > Time.now
+      errors[:birth_date] << 'must be in the past'
+    end
   end
 end
