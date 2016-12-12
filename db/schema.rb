954156e255763544aa1475004f359ec61e5380f5
Dave Charnuska <davecharnuska@gmail.com>
2016-12-12 12:08:19 -0500

    add email column to users model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209070452) do
+ActiveRecord::Schema.define(version: 20161212170623) do
 
   create_table "cat_rental_requests", force: :cascade do |t|
     t.integer  "cat_id",                           null: false
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20161209070452) do
     t.string   "password_digest", null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "email"
   end
 
   add_index "users", ["username"], name: "index_users_on_username", unique: true
